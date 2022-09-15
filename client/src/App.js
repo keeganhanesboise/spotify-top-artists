ad85cfbc9516c9a634b9fb7f03217de2b12c39c6
keeganhanesboise <70166916+keeganhanesboise@users.noreply.github.com>
2022-09-15 14:47:10 -0700

    Connected client to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Spotify Top Artists
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    console.log(params.access_token);
+  }
+
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.href.substring(23);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <a href='http://localhost:8888'>Login to Spotify</a>
+        <div id="artist-grid">
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
